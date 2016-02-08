13c16146f5d993b7dc5e0dedd11b8b6fcd624974
Nikita Bakaev <ya@nbakaev.ru>
2016-02-08 09:16:00 +0300

    upd

--- src/main/java/ru/hishop/good/Good.java
+++ src/main/java/ru/hishop/good/Good.java
@@ -1,7 +1,11 @@
 package ru.hishop.good;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 import ru.hishop.entity.BasicClassAbstract;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nikita Bakaev, ya@nbakaev.ru on 1/10/2016.
@@ -11,14 +15,57 @@ import ru.hishop.entity.BasicClassAbstract;
 @Document
 public class Good extends BasicClassAbstract {
 
+    @ApiModelProperty("Price of good as double")
     private double price;
 
-    /**
-     * true to show on / page
-     */
+    @ApiModelProperty("Number that can be bought")
+    private int numberAvailable;
 
+    @ApiModelProperty("Number that was sold")
+    private int numberSold;
+
+    @ApiModelProperty("Some user-defined description in html")
+    private String description;
+
+    @ApiModelProperty("Ids of categories")
+    private List<String> categoriesIds = new ArrayList<>();
+
+    @ApiModelProperty("true to show on / page")
     private boolean showOnIndexPage = false;
 
+
+
+    public int getNumberAvailable() {
+        return numberAvailable;
+    }
+
+    public void setNumberAvailable(int numberAvailable) {
+        this.numberAvailable = numberAvailable;
+    }
+
+    public int getNumberSold() {
+        return numberSold;
+    }
+
+    public void setNumberSold(int numberSold) {
+        this.numberSold = numberSold;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getCategoriesIds() {
+        return categoriesIds;
+    }
+
+    public void setCategoriesIds(List<String> categoriesIds) {
+        this.categoriesIds = categoriesIds;
+    }
 
     public boolean isShowOnIndexPage() {
         return showOnIndexPage;
--- src/main/java/ru/hishop/purchaseorder/PurchaseOrder.java
+++ src/main/java/ru/hishop/purchaseorder/PurchaseOrder.java
@@ -1,9 +1,9 @@
 package ru.hishop.purchaseorder;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 import ru.hishop.entity.BasicClassAbstract;
 import ru.hishop.good.Good;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +15,29 @@ import java.util.List;
 @Document
 public class PurchaseOrder extends BasicClassAbstract {
 
+    @ApiModelProperty("Price of good as double")
     private double price;
 
     /**
      * Does this order is full paid
      */
+    @ApiModelProperty("Is it paid")
     private boolean paid = false;
 
+    @ApiModelProperty("Is it refunded")
+    private boolean refunded = false;
+
+    @ApiModelProperty("Ids of goods in order")
     private List<Good> goodList = new ArrayList<>();
 
+
+    public boolean isRefunded() {
+        return refunded;
+    }
+
+    public void setRefunded(boolean refunded) {
+        this.refunded = refunded;
+    }
 
     public boolean isPaid() {
         return paid;
