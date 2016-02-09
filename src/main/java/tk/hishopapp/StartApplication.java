ec8b9b10ec64c2e00b126b08aba3479b229dcd22
Nikita Bakaev <ya@nbakaev.ru>
2016-02-09 16:48:31 +0300

    delete auto mongodb auto configuration

--- src/main/java/tk/hishopapp/StartApplication.java
+++ src/main/java/tk/hishopapp/StartApplication.java
@@ -3,13 +3,15 @@ package tk.hishopapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan("tk.hishopapp")
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {MongoDataAutoConfiguration.class, MongoAutoConfiguration.class})
 public class StartApplication {
 
 	public static void main(String[] args) {
