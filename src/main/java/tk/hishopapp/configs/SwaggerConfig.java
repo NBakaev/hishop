75329bfc6d7326eb8050c933cd55de41dcf57366
Nikita Bakaev <ya@nbakaev.ru>
2016-02-17 13:16:11 +0300

    ignore spring actuator endpoints

--- src/main/java/tk/hishopapp/configs/SwaggerConfig.java
+++ src/main/java/tk/hishopapp/configs/SwaggerConfig.java
@@ -16,10 +16,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfig {
 
+    // ignore spring actuator endpoints
+    private boolean checkInternal(String s) {
+        return !(s.startsWith("/internal") || s.startsWith("/error"));
+    }
+
     @Bean
     public Docket newsApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2).select().build();
+        docket = docket.select().paths(this::checkInternal).build();
+        return docket;
     }
 
     private springfox.documentation.service.ApiInfo apiInfo() {
