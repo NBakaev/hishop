8ab0019b67a392d94bcac37df39dd9f9a498de9f
Nikita Bakaev <ya@nbakaev.ru>
2016-02-21 16:23:55 +0300

    working test mongodb mock

--- src/main/java/tk/hishopapp/StartApplication.java
+++ src/main/java/tk/hishopapp/StartApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan("tk.hishopapp")
 @Configuration
-@EnableAutoConfiguration(exclude = {MongoDataAutoConfiguration.class, MongoAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {MongoDataAutoConfiguration.class, MongoAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class})
 public class StartApplication {
 
 	public static void main(String[] args) {
