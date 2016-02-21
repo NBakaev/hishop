b0925ea4730b60f7e3ec1ab1c8a36b1203ced695
Nikita Bakaev <ya@nbakaev.ru>
2016-02-21 17:32:29 +0300

    add javadoc description

--- src/main/java/tk/hishopapp/good/GoodRepository.java
+++ src/main/java/tk/hishopapp/good/GoodRepository.java
@@ -3,17 +3,47 @@ package tk.hishopapp.good;
 import java.util.List;
 
 /**
+ * Interface that specifies a basic set of Good operations in e-shop. Implemented by {@link GoodRepositoryImpl}.
+ * <p>
  * Created by Nikita Bakaev, ya@nbakaev.ru on 2/9/2016.
  * All Rights Reserved
  */
 public interface GoodRepository {
+
+    /**
+     * Create new good
+     *
+     * @param good
+     * @return
+     */
     Good createGood(Good good);
 
+    /**
+     * Get all gods
+     *
+     * @return
+     */
     List<Good> getAllGoods();
 
+    /**
+     * Update existing good
+     *
+     * @param good
+     * @return
+     */
     Good updateGood(Good good);
 
+    /**
+     * Delete existing good by id
+     *
+     * @param id
+     */
     void deleteGoodById(String id);
 
+    /**
+     * Get all goods which should be shown in UI (html) in main(root) page
+     *
+     * @return
+     */
     List<Good> getAllGoodsOnIndexPage();
 }
