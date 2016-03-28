e5c81e8a100ba0fcf05111c3b90e4bf048e305c7
Nikita Bakaev <ya@nbakaev.ru>
2016-03-28 11:51:27 +0300

    excel export controller

--- src/main/java/tk/hishopapp/good/GoodController.java
+++ src/main/java/tk/hishopapp/good/GoodController.java
@@ -1,13 +1,21 @@
 package tk.hishopapp.good;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tk.hishopapp.auth.UserAccountRoles;
+import tk.hishopapp.dto.GoodEntityDto;
+import tk.hishopapp.utils.parsing.excel.BasicExcelObjectPrinter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -20,10 +28,13 @@ import java.util.List;
 public class GoodController {
 
     private final GoodRepository goodRepository;
+    private final GoodExcelExportService goodExcelExportService;
+
 
     @Autowired
-    public GoodController(final GoodRepository goodRepository) {
+    public GoodController(final GoodRepository goodRepository, final GoodExcelExportService goodExcelExportService) {
         this.goodRepository = goodRepository;
+        this.goodExcelExportService = goodExcelExportService;
     }
 
     @Secured({UserAccountRoles.ROLE_ADMIN})
@@ -62,6 +73,35 @@ public class GoodController {
     @ResponseBody
     List<Good> getAllGoodsInIndexPage() {
         return goodRepository.getAllGoodsOnIndexPage();
+    }
+
+    // TODO:   @Secured({UserAccountRoles.ROLE_ADMIN})
+    @ApiOperation(value = "get goods in excel by criteria (builder)")
+    @RequestMapping(value = "excel/download", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    ResponseEntity<byte[]> getGoodsInExcel(@RequestBody GoodEntityDto goodEntityDto, HttpServletResponse response, HttpServletRequest request) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(BasicExcelObjectPrinter.EXCEL_MIME_TYPE));
+
+        ResponseEntity responseEntity = new ResponseEntity(goodRepository.getGoodsByDto(goodEntityDto), headers, HttpStatus.ACCEPTED);
+        return responseEntity;
+    }
+
+    // TODO:   @Secured({UserAccountRoles.ROLE_ADMIN})
+    @RequestMapping(value = "excel/download.xlsx", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity<byte[]> getAllGoodsInExcel() {
+
+        GoodEntityDto goodEntityDto = new GoodEntityDto();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(BasicExcelObjectPrinter.EXCEL_MIME_TYPE));
+
+        ResponseEntity responseEntity = new ResponseEntity(goodExcelExportService.getGoodsExcel(goodEntityDto), headers, HttpStatus.ACCEPTED);
+        return responseEntity;
     }
 
 }
