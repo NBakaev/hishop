d88ed7d1752e89c65ff0c741969f1f5aef573874
Nikita Bakaev <ya@nbakaev.ru>
2016-04-19 22:42:00 +0300

    set versions with semver build

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     cd $BASE_DIR
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
     mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
-#    exit $?
-#fi
+    exit $?
+fi
