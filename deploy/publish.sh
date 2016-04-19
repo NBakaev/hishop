22164eb5e19792b3b79de1141b5c287f6ab73e1e
Nikita Bakaev <ya@nbakaev.ru>
2016-04-19 21:39:50 +0300

    fix deploy

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    cd $BASE_DIR
     mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
