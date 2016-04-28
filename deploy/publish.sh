995860cc5559b4f0c1dd1233a2e85b6800abf297
Nikita Bakaev <ya@nbakaev.ru>
2016-04-28 21:00:25 +0300

    add version docker

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -3,10 +3,17 @@
 # fail on error
 set -e
 
-PROJECT_VERSION=""
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
 
-function deployDocker(){
-cd $BASE_DIR/docker/api-backend
+############# deployMaven
+
+    cd $BASE_DIR
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
+    mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
+    export PROJECT_VERSION = "`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -v '\['`"
+
+############# deployDocker
+    cd $BASE_DIR/docker/api-backend
 
 #    move jar artifact to docker build folder
     mv $BASE_DIR/target/ru.nbakaev.hishop.jar $BASE_DIR/docker/api-backend/release.jar
@@ -15,19 +22,6 @@ cd $BASE_DIR/docker/api-backend
     docker build --tag=nbakaev/hishop-api-backend:"$PROJECT_VERSION" .
 #    docker tag hishop-api-backend HUB/___
     docker push nbakaev/hishop-api-backend
-}
-
-function deployMaven(){
-cd $BASE_DIR
-    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
-    mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
-    PROJECT_VERSION = "`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -v '\['`"
-}
-
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-
-    deployMaven
-    deployDocker
 
     exit $?
 fi
