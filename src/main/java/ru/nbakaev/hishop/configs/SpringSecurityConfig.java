2b4020c0f403a541cb72192652a5388af24c034f
Nikita Bakaev <ya@nbakaev.ru>
2016-04-28 19:40:34 +0300

    replace spring xml with java config

--- src/main/java/ru/nbakaev/hishop/configs/SpringSecurityConfig.java
+++ src/main/java/ru/nbakaev/hishop/configs/SpringSecurityConfig.java
@@ -1,10 +1,17 @@
 package ru.nbakaev.hishop.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import ru.nbakaev.hishop.auth.LocalAuthenticationProvider;
 
 /**
  * Created by Nikita Bakaev, ya@nbakaev.ru on 1/10/2016.
@@ -12,13 +19,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
-@ImportResource({"classpath:webSecurityConfig.xml"})
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LocalAuthenticationProvider localAuthenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll()
-        ;
+
+        http.sessionManagement().disable().rememberMe().disable();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/actuator/**").hasRole("ROOT_ADMIN")
+                .antMatchers("/v2/api-docs/**").permitAll()
+                .antMatchers("/**").permitAll()
+                .and()
+                .httpBasic()
+                .and().csrf().disable();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(localAuthenticationProvider);
     }
 
 }
