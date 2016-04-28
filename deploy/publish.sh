e878a9ab241a2c85310375b000f24ec9f6ddf915
Nikita Bakaev <ya@nbakaev.ru>
2016-04-28 21:24:36 +0300

    add version docker

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -10,7 +10,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     cd $BASE_DIR
     mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
     mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
-    $PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -v '\['`"
+    PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -v '\['`"
 
 ############# deployDocker
     cd $BASE_DIR/docker/api-backend
