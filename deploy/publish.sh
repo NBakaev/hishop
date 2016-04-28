9f53187dcc2f7aef870dd566ae9c1f19e7f25912
Nikita Bakaev <ya@nbakaev.ru>
2016-04-28 21:54:42 +0300

    add version docker (+9 squashed commit)
    Squashed commit:
    
    [570a737] add version docker
    
    [3721575] add version docker
    
    [de5e715] add version docker
    
    [a07f4d5] add version docker
    
    [a44114b] add version docker
    
    [e878a9a] add version docker
    
    [01dae18] add version docker
    
    [995860c] add version docker
    
    [ca5dff7] add version docker

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -10,10 +10,11 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     cd $BASE_DIR
     mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
     mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
-    PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null | grep -v '\['`"
+    PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null |grep -Ev '(^\[|Download\w+:)'`"
+    PROJECT_VERSION_DOCKER="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null |grep -Ev '(^\[|Download\w+:)' | tr + _`"
 
-    echo "PROJECT_VERSION is"
-    echo $PROJECT_VERSION
+    echo "PROJECT_VERSION is $PROJECT_VERSION"
+    echo "PROJECT_VERSION_DOCKER is $PROJECT_VERSION_DOCKER"
 ############# deployDocker
     cd $BASE_DIR/docker/api-backend
 
@@ -21,7 +22,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     mv $BASE_DIR/target/ru.nbakaev.hishop.jar $BASE_DIR/docker/api-backend/release.jar
 
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-    docker build --tag=nbakaev/hishop-api-backend:{$PROJECT_VERSION} .
+    docker build --tag=nbakaev/hishop-api-backend:$PROJECT_VERSION_DOCKER .
 #    docker tag hishop-api-backend HUB/___
     docker push nbakaev/hishop-api-backend
 
