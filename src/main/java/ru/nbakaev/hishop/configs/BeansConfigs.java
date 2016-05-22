957e68fe9c4f890d1f8d97319e489e1d92040a7a
Nikita Bakaev <ya@nbakaev.ru>
2016-05-22 18:04:34 +0300

    selete unused bean

--- src/main/java/ru/nbakaev/hishop/configs/BeansConfigs.java
+++ src/main/java/ru/nbakaev/hishop/configs/BeansConfigs.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -15,11 +14,6 @@ public class BeansConfigs {
     RestTemplate restTemplate() throws Exception {
         RestTemplate restTemplate = new RestTemplate(new org.springframework.http.client.HttpComponentsClientHttpRequestFactory());
         return restTemplate;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
     }
 
 }
