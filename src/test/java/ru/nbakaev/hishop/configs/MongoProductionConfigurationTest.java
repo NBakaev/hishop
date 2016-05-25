988882fa1047eb8177c5d1e082d8f0bd5d2bba64
Nikita Bakaev <ya@nbakaev.ru>
2016-05-25 16:31:39 +0300

    better test mongodb bean

--- src/test/java/ru/nbakaev/hishop/configs/MongoProductionConfigurationTest.java
+++ src/test/java/ru/nbakaev/hishop/configs/MongoProductionConfigurationTest.java
@@ -1,11 +1,13 @@
 package ru.nbakaev.hishop.configs;
 
+import com.mongodb.CommandResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.nbakaev.hishop.StartApplication;
 
@@ -23,7 +25,10 @@ public class MongoProductionConfigurationTest extends AbstractTestNGSpringContex
 
     @Test
     public void testMongoProductionConfigurationTest() {
-        assert mongoTemplate != null;
+        CommandResult result = mongoTemplate.executeCommand("{ buildInfo: 1 }");
+
+        Assert.assertNotNull(result);
+        Assert.assertNotNull(result.getString("version"));
     }
 
 }
